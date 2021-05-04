4950fe05cb304612b8dad52086ab007c9ba98ecf
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-05-04 18:27:40 +0300

    feat: implement readable-transform-writable pipline functionality

--- cctool.js
+++ cctool.js
@@ -29,4 +29,46 @@ const options = program
   .parse()
   .opts();
 
-console.log(options);
+//console.log(options);
+
+class СaesarCipherTransform extends require('stream').Transform {
+  action = undefined;
+  shift = undefined;
+
+  constructor(action, shift) {
+    super();
+    this.action = action;
+    this.shift = shift;
+  }
+
+  _transform(chunk, encoding, callback) {
+    // Code to encode/decode chunk of text
+    this.push(`action = ${this.action}, `);
+    this.push(`shift = ${this.shift}\n`);
+    this.push(chunk.toString().toUpperCase());
+    callback();
+  }
+};
+
+const transform = new СaesarCipherTransform(options.action, options.shift);
+
+// process.stdin.on('data', () => {
+//   process.stdin.read();
+// });
+//
+// process.stdin.on('end', () => {
+//   process.stdout.write('end!!!');
+// });
+//
+//process.stdin.pipe(transform).pipe(process.stdout);
+
+const readable = process.stdin;
+const writabe = process.stdout;
+
+const { pipeline } = require('stream');
+pipeline(
+  readable,
+  transform,
+  writabe,
+  (err) => { console.error('error', err) },
+);
