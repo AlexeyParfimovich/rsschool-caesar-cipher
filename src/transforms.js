8b4a1aefd9ec1242341671dab76e4aabccfb56ad
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-05-05 17:34:41 +0300

    feat: implement cipher algorithm running both directions for any integer Shift

--- src/transforms.js
+++ src/transforms.js
@@ -10,8 +10,28 @@ class СaesarCipherTransform extends require('stream').Transform {
 
   _transform(chunk, encoding, callback) {
     // Code to encode/decode chunk of text
-    this.push(`${this.action}d with shift on ${this.shift}:\n`);
-    this.push(chunk.toString().toUpperCase());
+    //this.push(`${this.action}d with shift on ${this.shift}:\n`);
+
+    if(this.action === 'decode') this.shift = -this.shift;
+    
+    this.push(chunk.toString().replace(/[A-Za-z]/g, (ch) => {
+      if(ch === ch.toUpperCase()){
+        if(this.shift >= 0) return String.fromCharCode((ch.charCodeAt(0) - 65 + this.shift) % 26 + 65);
+        else {
+          const code = (ch.charCodeAt(0) - 65 + this.shift) % 26;
+          if(code >= 0) return String.fromCharCode(code + 65);
+          else return String.fromCharCode(code + 26 + 65);
+        }  
+      } else {
+        if(this.shift >= 0) return String.fromCharCode((ch.charCodeAt(0) - 97 + this.shift) % 26 + 97);
+        else {
+          const code = (ch.charCodeAt(0) - 97 + this.shift) % 26;
+          if(code >= 0) return String.fromCharCode(code + 97);
+          else return String.fromCharCode(code + 26 + 97);
+        }
+      } 
+    }));
+
     callback();
   }
 };
